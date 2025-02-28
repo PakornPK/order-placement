299a94aa011a662234f48ad138ecd4fc86ed358b
PakornPK <sweetie_jimmie@hotmail.com>
2025-03-01 01:40:40 +0700

    complete api

--- handler/order.go
+++ handler/order.go
@@ -1,19 +1,32 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	dto "github.com/PakornPK/order-placement/Dto"
+	"github.com/PakornPK/order-placement/service"
+	"github.com/gofiber/fiber/v2"
+)
 
 type OrderHandler interface {
 	PlaceOrderHandler(c *fiber.Ctx) error
 }
 
 type orderHandler struct {
+	serv service.OrderService
 }
 
-func NewOrderHandler() OrderHandler {
-	return orderHandler{}
+func NewOrderHandler(serv service.OrderService) OrderHandler {
+	return orderHandler{serv: serv}
 }
 
 func (h orderHandler) PlaceOrderHandler(c *fiber.Ctx) error {
-	
-	return c.Status(fiber.StatusOK).JSON(map[string]string{"status": "OK"})
+	req := make([]dto.InputOrder, 0)
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(nil)
+	}
+
+	res, err := h.serv.PlaceOrder(req)
+	if err != nil {
+		return c.Status(fiber.StatusConflict).JSON(nil)
+	}
+	return c.Status(fiber.StatusOK).JSON(res)
 }
--- route/route.go
+++ route/route.go
@@ -3,10 +3,12 @@ package route
 import (
 	"github.com/PakornPK/order-placement/handler"
 	"github.com/PakornPK/order-placement/logs"
+	"github.com/PakornPK/order-placement/service"
 	"github.com/gofiber/fiber/v2"
 )
 
 func New(app *fiber.App, logger logs.Logger) {
-	hand := handler.NewOrderHandler()
+	serv := service.NewOrderService()
+	hand := handler.NewOrderHandler(serv)
 	app.Post("/order", hand.PlaceOrderHandler)
 }
